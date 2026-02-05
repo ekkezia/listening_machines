0543f954923026df13d7b96f7dbb64be9245167b
Elizabeth Kezia <91784486+ekkezia@users.noreply.github.com>
2026-02-05 09:49:29 -0500

    homework

--- class_1/radio_rex/radio_rex.ino
+++ class_1/radio_rex/radio_rex.ino
@@ -1,4 +1,4 @@
-// const int LED_PIN = 3; // built-in LED on most Arduinos
+const int LED_PIN = 3; // built-in LED on most Arduinos
 
 // void setup() {
 //     Serial.begin(9600);
@@ -29,6 +29,7 @@ int val;    // variable to read the value from the analog pin
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
@@ -36,10 +37,12 @@ void loop() {
         char received = Serial.read();
 
         if (received == '1') {
-            myservo.write(received * 100);   
+            myservo.write(received * 100);  
+            digitalWrite(LED_PIN, HIGH); 
         }
         else if (received == '0') {
             myservo.write(0);
+            digitalWrite(LED_PIN, HIGH);
         }
     }
 }
